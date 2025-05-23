e75d1eff8c378f5cd520415f3ecb9168f9c52727
Juani-hb <juanihernandezbusquet@gmail.com>
2025-05-23 12:10:55 -0300

    conexiondb

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import cors from "cors";
+import './db.js';
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(cors());
 app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`EVA is listening at http://localhost:${port}`);
+});
