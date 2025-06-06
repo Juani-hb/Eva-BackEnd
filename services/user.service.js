44ad6a2ce05feee8a42f676efbb9a58a4a7bb63b
Juani-hb <juanihernandezbusquet@gmail.com>
2025-06-06 11:55:26 -0300

    ahorasilogin

--- services/user.service.js
+++ services/user.service.js
@@ -3,7 +3,7 @@ import db from '../db.js';
 export const verificarLogin = async (email, contra) => {
   const result = await db.query(
     'SELECT * FROM "User" WHERE email = $1 AND contra = $2  ' ,
-    [email, contra, usuario]
+    [email, contra]
   );
 
   return result.rows.length > 0;
