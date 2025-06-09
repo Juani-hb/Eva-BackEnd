d35b76ddcc2cbe287391e33fbb962c75884b99eb
Juani-hb <juanihernandezbusquet@gmail.com>
2025-06-09 15:01:07 -0300

    conexioncloudinary

--- upload.js
+++ upload.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: "<cloudname>",
-  api_key: "<api_key>",
-  api_secret: "<api_secret>"
+  cloud_name: "dwka1bnz1",
+  api_key: "488132112127876",
+  api_secret: "pYOqYW24qXjw"
 });
 
 export default cloudinary;
