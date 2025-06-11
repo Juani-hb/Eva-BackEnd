305b80ffb735e7c5879d2ae72015c4bd2d3b95bb
Juani-hb <juanihernandezbusquet@gmail.com>
2025-06-11 15:58:33 -0300

    commit con errores

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import verificarLogin from "../services/user.service.js"
+
+export const Token = async (res, req, next) => {
+
+try
+{
+if(!req.headers. )
+
+}
+catch
+{
+
+
+}
+
+
+};
