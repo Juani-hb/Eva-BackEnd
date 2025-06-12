a732e15a0f4fca0e2ab92a946373d8eae7302caf
Juani-hb <juanihernandezbusquet@gmail.com>
2025-06-12 15:06:53 -0300

    auth middleware

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,18 +1,29 @@
 import jwt from "jsonwebtoken";
-import verificarLogin from "../services/user.service.js"
+import MedicoService from "../services/medico.service.js";
 
-export const Token = async (res, req, next) => {
+export const verifyToken = async (req, res, next) => {
 
-try
-{
-if(!req.headers. )
+    try{
+        if(!req.headers.authorization)
+            return res.status(401).json({message: "No puede acceder."});
 
-}
-catch
-{
+        const token = req.headers.authorization.split(" ")[1];
 
+        if (!token) 
+            return res.status(400).json({message: "Formato invalido de token."});
 
-}
+        const payload = await jwt.verify(token, "secret");
 
+        if (!payload.id) 
+            return res.status(400).json({ message: "El token no contiene un ID de usuario."});
+    
+        req.id = parseInt(payload.id);
+
+        next();
+
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
 
 };
+
