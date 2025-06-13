740abac8ccd112bc5d0479bf7478652a9d6f32fd
Juani-hb <juanihernandezbusquet@gmail.com>
2025-06-12 23:18:30 -0300

    error2

--- routes/videos.router.js
+++ routes/videos.router.js
@@ -2,7 +2,6 @@ import multer from 'multer';
 import videoController  from "../controllers/videos.controller.js";
 import { verifyToken } from "../middlewares/auth.middlewares.js";
 import { Router } from "express";
-import multer from 'multer';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
